38082c293fa2154cb3db5aec5e58536aa607bc35
Tatsuya Shiba <justin-ruediger@gmx.de>
2022-07-25 22:06:17 +0200

    first commit

---
+++ start.bat
@@ -0,0 +1,6 @@
+@echo off
+echo [Hector Seminar Project]
+echo 1. Open a Hotspot on your PC
+echo 2, Connect a mobile device like phone or tablet to the hotspot on your PC
+echo 3. scan the QR-code to start the app
+zapworks serve --lan
